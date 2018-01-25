35c155847dad6ac1d00081298f6dfab0b86c87fa
David Gassner <dgassnerdev@gmail.com>
2018-01-25 15:43:37 -0800

    Branch 01_05 complete

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,4 @@
 fun main(args: Array<String>) {
-    println("Welcome to Kotlin Essential Training!")
+    println("Welcome to Kotlin Essential Training, ${args[0]}!")
+    println("The first argument is " + args[0])
 }
