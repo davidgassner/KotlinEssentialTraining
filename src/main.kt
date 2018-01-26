3e74cfbcb2b169a6b30a25a455f0a0305ccf7128
David Gassner <dgassnerdev@gmail.com>
2018-01-26 09:48:41 -0800

    Initial commit

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,3 @@
 fun main(args: Array<String>) {
-    println("Welcome to Kotlin Essential Training, ${args[0]}!")
-    println("The first argument is " + args[0])
+    println("Welcome to Kotlin Essential Training!")
 }
