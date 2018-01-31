ee0871f0332144cbd4b02add0a5c5abf3318b36c
David Gassner <dgassnerdev@gmail.com>
2018-01-31 12:13:36 -0800

    03_03 End

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,16 @@
 fun main(args: Array<String>) {
-    println("Welcome to Kotlin Essential Training!")
+    val myInt = 42
+    val myLong:Long = myInt.toLong()
+
+    println("the type of myLong is ${myLong::class.qualifiedName}")
+
+    val myLong2 = 42.9
+    var myInt2 = myLong2.toInt()
+    println("the value of myLong2 is $myLong2")
+    println("the value of myInt2 is $myInt2")
+
+    val myInt3 = 568
+    val myDouble3 = myInt3.toDouble()
+    println("the value of myDouble3 is $myDouble3")
+
 }
