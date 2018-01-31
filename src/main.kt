0839a05c53d40a4046f6f84e36bf8b0253f128de
David Gassner <dgassnerdev@gmail.com>
2018-01-31 12:32:10 -0800

    03_04 End

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,12 @@
 fun main(args: Array<String>) {
-    val num1 = 15
-    val num2 = 10
+    val num1 = 10
+    val num2 = 15
+
+    val match = (num1 == num2)
+    println("Match = $match")
+
+//    val match2 = num1.equals(num2)
+//    println("Match2 = $match2")
+
+    println("Comparison result = ${num1.compareTo(num2)}")
 }
