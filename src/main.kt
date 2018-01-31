3955f786b92f44b73694551201b56f16b5e048dd
David Gassner <dgassnerdev@gmail.com>
2018-01-31 11:13:06 -0800

    03_01 End

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,13 @@
-fun main(args: Array<String>) {
+val myName = "David"
 
+fun main(args: Array<String>) {
+    var num1 = 45.0
+    println("The initial value of num1 is $num1")
+    num1 ++
+    println("The new value of num1 is $num1")
+
+    val num2:Double = 5.toDouble()
+    println("The value of num2 is $num2")
+
+    println("My name is $myName")
 }
