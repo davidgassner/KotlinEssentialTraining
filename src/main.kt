d54cc370cfbcb7a5d264a544e4cb7104a120c8d7
David Gassner <dgassnerdev@gmail.com>
2018-02-01 12:11:54 -0800

    03_06 Start

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,20 @@
 fun main(args: Array<String>) {
-    println("Welcome to Kotlin Essential Training!")
+    val num1 = 15
+    val num2 = 10
+
+    val sum = num1.plus(num2)
+    println("sum=$sum")
+
+    val difference = num1.minus(num2)
+    println("difference=$difference")
+
+    val product = num1.times(num2)
+    println("product=$product")
+
+    val quotient:Double = num1.toDouble().div(num2)
+    println("quotient=$quotient")
+
+    val remainder = num1.rem(num2)
+    println("remainder=$remainder")
+
 }
