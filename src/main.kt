a894a9a29eb071f6eef8c0e783a4ef89a378601e
David Gassner <dgassnerdev@gmail.com>
2018-02-01 11:54:17 -0800

    03_05 End

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,22 @@
 fun main(args: Array<String>) {
-    val num1 = 15
+    var num1 = 15
     val num2 = 10
+
+    val sum = num1 + num2
+    println("sum: $sum")
+    val sum2 = num1.plus(num2)
+    println("sum2: $sum2")
+
+    val diff = num1 - num2
+    println("diff: $diff")
+
+    num1 ++
+    println("num1: $num1")
+
+    val num3 = num1.inc()
+    println("num1: $num3")
+
+    println("the value of n1 = ${++num1}")
+    println ("num1=$num1")
+
 }
