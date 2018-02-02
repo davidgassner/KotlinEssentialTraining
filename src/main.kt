bb1b82b3223baf7c01eae2c53fe2e2be483a64ee
David Gassner <dgassnerdev@gmail.com>
2018-02-02 11:54:33 -0800

    03_08 End

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,9 @@
 fun main(args: Array<String>) {
+    val builder = StringBuilder("To be or not to be\n")
+            .append("that is the question\n")
+            .append("Whether 'tis nobler in the mind\n")
+            .append("to suffer the slings and arrows")
 
+    val result = builder.toString()
+    println(result)
 }
