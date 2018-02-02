cc03b03be6bfae70c7510c5d8eb5cb055a3854cd
David Gassner <dgassnerdev@gmail.com>
2018-02-02 11:47:42 -0800

    03_07 End

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,29 @@
 fun main(args: Array<String>) {
+    var aString = "Hello!"
+    println(aString)
 
+    val empty = String()
+    println("'$empty'")
+
+    val charArray = aString.toCharArray()
+    println(charArray.toList())
+    val byteArray = aString.toByteArray()
+    println(byteArray.toList())
+
+    aString += " and Welcome!"
+    println(aString)
+
+    val len = aString.length
+    for (i in 0 until len) {
+        val c = aString.get(i)
+        println(c)
+    }
+
+    val p = aString.indexOf("W")
+    val sub = aString.substring(p)
+    println(sub)
+
+    val string2 = aString.toUpperCase()
+    val match = aString.equals(string2, true)
+    println("Do they match? $match")
 }
