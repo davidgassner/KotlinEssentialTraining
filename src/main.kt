73764154652b29be7d818df9777b080d4d672f84
David Gassner <dgassnerdev@gmail.com>
2018-02-05 13:29:13 -0800

    04_06 End

--- src/main.kt
+++ src/main.kt
@@ -1,6 +1,20 @@
 fun main(args: Array<String>) {
     val states = arrayOf("CA", "OR", "WA")
 
+    printHeader("While loop")
+    var counter = 0
+    while (counter < states.size) {
+        println("Counter = $counter")
+        println("State = ${states.get(counter)}")
+        counter ++
+    }
+
+    counter = 0
+    printHeader("Do / While loop")
+    do {
+        println("State = ${states.get(counter)}")
+        counter ++
+    } while (counter < states.size)
 }
 
 fun printHeader(label: String) {
