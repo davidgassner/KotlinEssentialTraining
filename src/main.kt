d037870f733f96ac4e799cd5cb1ebc3fce23c225
David Gassner <dgassnerdev@gmail.com>
2018-02-05 11:25:17 -0800

    04_04 End

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,29 @@
 fun main(args: Array<String>) {
+    var nullValue:String? = null
+    println(nullValue)
+
+    val l:Int? = nullValue?.length
+    println("Length is $l")
+
+    if (l == null) {
+        println("Length is null")
+    }
+
+    val message = if (l != null) {
+        "Length: $l"
+    } else {
+        "l is null"
+    }
+    println(message)
+
+    val l2 = l ?: -1
+    println("The value of l2 is $l2")
+
+    try {
+        val l3 = l!!
+        println("The value of l3 is $l3")
+    } catch (e: KotlinNullPointerException) {
+        println("l3 is null")
+    }
 
 }
