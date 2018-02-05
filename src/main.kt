9a80c01d69b7c4ae90cf3de2e1aba36274609af1
David Gassner <dgassnerdev@gmail.com>
2018-02-05 09:36:53 -0800

    04_03 Start

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,14 @@
 fun main(args: Array<String>) {
-    println("Welcome to Kotlin Essential Training!")
+    print("Enter a state abbreviation: ")
+    val state = readLine()
+
+    if (state == "CA") {
+        println("The capital is Sacramento")
+    }
+    else if (state == "OR") {
+        println("The capital is Salem")
+    }
+    else {
+        println("I don't know that state")
+    }
 }
