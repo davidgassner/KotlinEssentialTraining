fd8e7ad43b3bd43503b01d789fcf54c5ea1decfd
David Gassner <dgassnerdev@gmail.com>
2018-02-05 09:35:39 -0800

    04_02 End

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,8 @@
 fun main(args: Array<String>) {
+    print("Enter a state abbreviation: ")
+    val state = readLine()
 
+    if (state == "CA") println("The capital is Sacramento")
+    else if (state == "OR") println("The capital is Salem")
+    else println("I don't know that state")
 }
