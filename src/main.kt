0ce4fb5810ff39226b4da8b81e484df2e97e5879
David Gassner <dgassnerdev@gmail.com>
2018-02-05 11:48:17 -0800

    04_05 End

--- src/main.kt
+++ src/main.kt
@@ -2,6 +2,21 @@ fun main(args: Array<String>) {
     val colors = arrayOf("Red", "Green", "Blue")
     val values = intArrayOf(1, 3, 5, 7, 9)
 
+    printHeader("For each loop")
+    for (color in colors) {
+        println(color)
+    }
+    for (value in values) {
+        println(value)
+    }
+
+    printHeader("For loop with indices")
+    for (i in values.indices step 2) {
+        println(values[i])
+    }
+    for (i in 0 until colors.size) {
+        println(colors[i])
+    }
 }
 
 fun printHeader(label: String) {
