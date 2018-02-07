40e05cbec7db0487c728f59c28aa0d59b2e6e464
David Gassner <dgassnerdev@gmail.com>
2018-02-07 15:23:29 -0800

    06_06 Start

--- src/main.kt
+++ src/main.kt
@@ -3,14 +3,14 @@ import model.ClothingItem
 fun main(args: Array<String>) {
 
     val cart = mutableMapOf<ClothingItem, Int>()
-    cart[ClothingItem("Shirt", "XL", 14.99)] = 2
-    cart[ClothingItem("Pants", "32", 19.99)] = 4
-    cart[ClothingItem("Hat", "8.5", 24.00)] = 1
+    cart.put(ClothingItem("Shirt", "XL", 14.99), 2)
+    cart.put(ClothingItem("Pants", "32", 19.99), 4)
+    cart.put(ClothingItem("Hat", "8.5", 24.00), 1)
     println(cart)
 
     var total = 0.0
     for ((item, quantity) in cart) {
-        val itemTotal:Double = quantity * item.price
+        val itemTotal: Double = quantity * item.price
         println("Item ${item.type} @ ${item.price} = $itemTotal")
         total += itemTotal
     }
