9152db72c0c1c2ab0ef9e22aeabff03b55f05e19
David Gassner <dgassnerdev@gmail.com>
2018-02-07 11:08:35 -0800

    06_02 End

--- src/main.kt
+++ src/main.kt
@@ -1,2 +1,27 @@
 fun main(args: Array<String>) {
+
+    val colorList = listOf("Red", "Green", "Blue")
+    println(colorList)
+
+    println("Number of colors: ${colorList.size}")
+    println(colorList::class.simpleName)
+
+    val colorList2 = mutableListOf<String>()
+    colorList2.add("Red")
+    colorList2.add("Green")
+    colorList2.add("Blue")
+    println(colorList2)
+
+    colorList2.sort()
+    println(colorList2)
+
+    val sortedList = colorList2.sortedDescending()
+    println(sortedList)
+
+    colorList2.removeAt(0)
+    println(colorList2)
+
+    colorList2.remove("Red")
+    println(colorList2)
+
 }
