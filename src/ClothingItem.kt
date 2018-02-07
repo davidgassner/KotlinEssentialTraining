7b9d5a5a1fcf718022bf8af366335298047b323f
David Gassner <dgassnerdev@gmail.com>
2018-02-07 09:15:07 -0800

    05_06 Start

--- src/ClothingItem.kt
+++ src/ClothingItem.kt
@@ -1,12 +1,10 @@
-data class ClothingItem (var type: String?,
-                         val size: String,
-                         var price: Double) {
+data class ClothingItem(var type: String?,
+                        val size: String,
+                        var price: Double) {
     init {
         type = type?.toUpperCase() ?: "UNKNOWN"
     }
 
-    constructor(size: String, price: Double) : this(null, size, price){
-//        type = "Unknown"
-    }
+    constructor(size: String, price: Double) : this(null, size, price)
 
 }
