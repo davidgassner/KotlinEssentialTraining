9a5f39054506226895b77733e5b8c31fc59d100a
David Gassner <dgassnerdev@gmail.com>
2018-02-07 09:40:38 -0800

    Master

--- src/ClothingItem.kt
+++
@@ -1,3 +0,0 @@
-data class ClothingItem(val type: String,
-                        val size: String,
-                        var price: Double)
--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,3 @@
 fun main(args: Array<String>) {
-    val item = ClothingItem("Shirt", "L", 19.99)
-    println(item)
-
-    item.price = 15.99
-    println(item)
+    println("Welcome to Kotlin Essential Training!")
 }
