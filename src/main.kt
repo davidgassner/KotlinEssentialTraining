02eed04a9ba5cc342458ed27bfec7541637d8727
David Gassner <dgassnerdev@gmail.com>
2018-02-07 10:54:21 -0800

    06_01 End

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,39 @@
 fun main(args: Array<String>) {
 
+    val array1 = arrayOf("Red", "Green", "Blue")
+    for (element in array1) {
+        println(element)
+    }
+    val mixed = arrayOf("A String", 12)
+    for (element in mixed) {
+        println(element)
+    }
+
+    val nulls = arrayOfNulls<String>(3)
+    for (element in nulls) {
+        println(element)
+    }
+    nulls[0] = "Red"
+    nulls[1] = "Blue"
+    nulls[2] = "Green"
+    for (element in nulls) {
+        println(element)
+    }
+
+    val intArray = intArrayOf(3, 4, 5)
+    for (element in intArray) {
+        println(element)
+    }
+
+    array1.sort()
+    for (element in array1) {
+        println(element)
+    }
+
+    val array4 = array1.sortedArrayDescending()
+    for (element in array4) {
+        println(element)
+    }
+
+
 }
