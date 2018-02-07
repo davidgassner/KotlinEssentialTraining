4849dc8614c929c64ed93a5472b4f45676c1e9ba
David Gassner <dgassnerdev@gmail.com>
2018-02-07 14:18:45 -0800

    06_03 End

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,24 @@
-fun main(args: Array<String>) {
+import model.ClothingItem
 
+fun main(args: Array<String>) {
+    var colorSet = mutableSetOf("Red", "Green", "Blue")
+    println(colorSet)
+
+    colorSet.add("Purple")
+    colorSet.remove("Green")
+    println(colorSet)
+
+    val colorList = colorSet.toMutableList()
+    colorList.removeAt(0)
+    println(colorList)
+    colorList.add("Purple")
+    println(colorList)
+
+    val newSet = colorList.toMutableSet()
+    println(newSet)
+
+    val itemSet = mutableSetOf<ClothingItem>()
+    itemSet.add(ClothingItem("Shirt", "XL", 14.99))
+    itemSet.add(ClothingItem("Shirt", "M", 14.99))
+    println(itemSet)
 }
