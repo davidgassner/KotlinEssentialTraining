ea6251170c45a04f7b1bfa3413cd9e97f90c7836
David Gassner <dgassnerdev@gmail.com>
2018-02-08 11:21:08 -0800

    07_03 End

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,25 @@
 fun main(args: Array<String>) {
+    val stateful = StatefulClass(object : ClickListener {
+        override fun onClick(event: ClickEvent) {
+            println("Click at ${event.x}, ${event.y}")
+        }
+    })
+    println("Listener initialized")
+    stateful.clickMe(5, 18)
+    stateful.clickMe(45, 57)
 
+}
+
+class ClickEvent(val x: Int, val y: Int)
+
+interface ClickListener {
+    fun onClick(event: ClickEvent)
+}
+
+class StatefulClass(listener: ClickListener) {
+    private var _clickListener:ClickListener? = listener
+
+    fun clickMe(x: Int, y: Int) {
+        _clickListener?.onClick(ClickEvent(x, y))
+    }
 }
