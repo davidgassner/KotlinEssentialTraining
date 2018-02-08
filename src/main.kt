3d05abc4d85f76965e0baba5d15c728bfb42a6f2
David Gassner <dgassnerdev@gmail.com>
2018-02-08 10:18:43 -0800

    07_02 End

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,41 @@
 fun main(args: Array<String>) {
 
+    val buster = Retriever()
+//    buster.speak()
+    makeItTalk(buster)
+    reportBreed("Buster", buster)
+}
+
+fun makeItTalk(dog: Retriever) {
+    dog.speak()
+}
+
+fun reportBreed(name: String, dog: Dog) {
+    println("$name is a ${dog::class.simpleName}")
+    println("This dog's fur is ${dog.fur}")
+}
+
+interface Dog {
+    var fur: String
+    fun speak() {
+        println("Woof!")
+    }
+}
+
+interface Cat{
+    var fur: String
+    fun speak() {
+        println("Meow!")
+    }
+}
+
+class Retriever : Dog, Cat {
+    override var fur: String
+        get() = "golden"
+        set(value) {}
+
+    override fun speak() {
+        super<Dog>.speak()
+        super<Cat>.speak()
+    }
 }
