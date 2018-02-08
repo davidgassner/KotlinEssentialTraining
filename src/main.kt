52cc2b255da59b31de4d8730e06614a5e21eba8f
David Gassner <dgassnerdev@gmail.com>
2018-02-08 09:32:56 -0800

    07_01 End

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,38 @@
 fun main(args: Array<String>) {
+    val x:Any = Any()
+    println(x)
+
+    val sup = SuperClass(42)
+    println(sup)
+
+    val sub = SubClass(53)
+    println(sub)
+
+    println(sup.multiply(100))
+    println(sub.multiply(100))
 
 }
+
+open class SuperClass(anInt: Int){
+    protected val _anInt = anInt
+
+    override fun toString(): String {
+        return "${this::class.simpleName} {antInt: $_anInt}"
+    }
+
+    open fun multiply(factor: Int): Int {
+        return _anInt * factor
+    }
+}
+
+class SubClass(anInt:Int): SuperClass(anInt) {
+//    override fun toString(): String {
+//        return "SubClass{anInt: $_anInt}"
+//    }
+
+    override fun multiply(factor: Int): Int {
+        return super.multiply(factor) * factor
+    }
+}
+
+
