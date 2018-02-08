873e9d85ce5c992c23c33f638bab86bef8f5c9e2
David Gassner <dgassnerdev@gmail.com>
2018-02-08 12:22:56 -0800

    07_04 End

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,22 @@
 fun main(args: Array<String>) {
 
+    val helloWorld = {
+        println("Hello world!")
+    }
+    helloWorld()
+    helloWorld.invoke()
+
+    val sayHello = {
+        user: String, age: Int -> println("Hello, $user, you're $age years old!")
+    }
+    sayHello("David", 39)
+
+    val states = arrayOf("New York", "New Hampshire", "Vermont",
+            "Rhode Island", "Nebraska")
+//    val sorted = states.sortedBy { state -> state.length }
+    val sorted = states.sortedBy { it.length }
+            .filter { it.startsWith("n", true) }
+    for (state in sorted) {
+        println(state)
+    }
 }
