8052e18dc8e61e7759b0cf85ce35272195450574
David Gassner <dgassnerdev@gmail.com>
2018-02-09 09:43:24 -0800

    07_05 End

--- src/main.kt
+++ src/main.kt
@@ -1,8 +1,19 @@
 import model.ClothingItem
+import model.Pants
+import model.Shirt
 
 fun main(args: Array<String>) {
 
-    val item = ClothingItem("Shirt", "XL", 19.99)
-    println(item)
+    val item1 = Shirt("XL", 19.99)
+    val item2 = Pants("32", 24.99)
+
+    val mostExpensive: ClothingItem =
+            if (item1.price > item2.price) item1 else item2
+
+    val instructions = when(mostExpensive) {
+        is Shirt -> "Button it!"
+        is Pants -> "Buckle it!"
+    }
+    println(instructions)
 
 }
--- src/model/ClothingItem.kt
+++ src/model/ClothingItem.kt
@@ -1,3 +1,12 @@
 package model
 
-data class ClothingItem(val type: String, val size: String, val price: Double)
+sealed class ClothingItem(val type: String) {
+    abstract val size: String
+    abstract val price: Double
+}
+
+data class Shirt(override var size:String,
+                 override var price: Double): ClothingItem("Shirt")
+
+data class Pants(override var size:String,
+                 override var price: Double): ClothingItem("Pants")
